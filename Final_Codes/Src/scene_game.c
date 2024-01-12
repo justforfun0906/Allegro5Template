8b3fb02c9d8d28571eebcc5a24862389e2e9490e
justforfun0906 <jimmy0906abc@gmail.com>
2024-01-13 00:51:41 +0800

    failed

--- Final_Codes/Src/scene_game.c
+++ Final_Codes/Src/scene_game.c
@@ -1,5 +1,6 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_native_dialog.h>
 #include <stdio.h>
 #include <string.h>
 #include "game.h"
